3382602638938d60a65c093623bcd340fb5bc18f
Manu214609 <150542974+Manu214609@users.noreply.github.com>
2024-03-10 18:03:36 +0000

    proyecto listo

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -5,9 +5,9 @@ const Jumbotron = (props) => {
     
      <div className="p-5 mb-4 bg-body-tertiary rounded-3">
       <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold">A Warm Welcome</h1>
-        <p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-        <button className="btn btn-primary btn-lg" type="button">Call to Action</button>
+        <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
+        <p className="col-md-8 fs-4">En aquellos días, todos los jefes de los sacerdotes y el pueblo multiplicaron sus infidelidades, según las costumbres abominables de los gentiles, y mancharon la casa del Señor, que él se había construido en Jerusalén. El Señor, Dios de sus padres, les envió desde el principio avisos por medio de sus mensajeros, porque tenía compasión de su pueblo y de su morada.</p>
+        <button className="btn btn-primary btn-lg" type="button">Call to Action!</button>
       </div>
     </div>
     );
